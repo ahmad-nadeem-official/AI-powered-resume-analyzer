7a78773e14bbeb85676f9da568ec801b0bdcd360
ahmad-nadeem-official <ahmadnadeem701065@gmail.com>
2026-09-24 20:23:47 +0500

    Image error fixing

--- configuration/configur.py
+++ configuration/configur.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
+
 temp = 0.1
 llm = "openai/gpt-oss-120b"
 max_tokens = None
 reasoning_format="parsed"
 rate_limiter=None
 max_retries=3
-image = "/home/ahmad/workshop/AI-powered-resume-analyzer/resources/logo/Gemini_Generated_Image_moo2wamoo2wamoo2-removebg-preview.png"
+
+
+########imahe error fixing##############
+BASE_DIR = Path(__file__).resolve().parent.parent
+image = BASE_DIR / "resources" / "logo" / "Gemini_Generated_Image_moo2wamoo2wamoo2-removebg-preview.png"
